9c3e8634aaaa0f92f81a22ebd90b927ea8e779ec
jahidulkanchan <mdj524156@gmail.com>
2023-06-14 14:47:45 +0600

    try

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+
+
+
+// toggle nav bar
+const toggleBar = document.getElementById('toggle-bar');
+const menuBar = document.getElementById('top-menu')
+toggleBar.addEventListener('click', function(){
+  menuBar.classList.toggle('active')
+})
+document.addEventListener('click', (e)=>{
+  if(e.target.id !== 'top-menu' && e.target.id !== 'toggle-bar'){
+    menuBar.classList.remove('active')
+  }
+})
+
+//scroll nav
+const header = document.getElementById('header')
+window.addEventListener('scroll',()=>{
+if(scrollY > 100){
+header.classList.add('active')
+}
+else{
+  header.classList.remove('active')
+}
+})
+//owl-carousel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  dots: false,
+  margin:10,
+  autoplay:true,
+  autoplaySpeed:1500,
+  autoplayTimeout: 4000,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:1
+      }
+  }
+})
+//counter
+$('.counterup').counterUp({
+  delay: 10,
+  time: 500
+});
+// wow js
+new WOW().init();
